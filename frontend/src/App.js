4124a76cbc4b68e0bd77936ce67c12f3996f56d4
Burekslayer <ivanscvetkovic@gmail.com>
2025-08-13 01:32:01 +0200

    frontend test

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React with Čuča and Maki, što vam kevu safti
         </a>
       </header>
     </div>
