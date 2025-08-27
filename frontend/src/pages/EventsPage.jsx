8a576918901ffe55e19845743ff50da86c5a5dd8
Burekslayer <ivanscvetkovic@gmail.com>
2025-08-27 18:07:05 +0200

    frontend getAllEvents

--- frontend/src/pages/EventsPage.jsx
+++ frontend/src/pages/EventsPage.jsx
@@ -1,6 +1,28 @@
 import Header from "../partials/Header";
 
 function EventsPage() {
+
+  const getAllEvents = async () => {
+    try {
+      const res = await fetch("http://localhost:5000/api/events/all", {
+        method: "GET",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(form),
+      });
+
+      if (!res.ok) {
+        const data = await res.json().catch(() => ({}));
+        throw new Error(data.error || `Request failed: ${res.status}`);
+      }
+
+      return (saved = await res.json());
+    } catch (e2) {
+      setErr(e2.message);
+    }
+  };
+
+  console.log(getAllEvents());
+
   return (
     <div>
       <Header />
